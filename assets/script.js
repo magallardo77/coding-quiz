983bf9971ff4ad70ae27699d32dc5a3388dffd3b
Miguel Gallardo <110438303+magallardo77@users.noreply.github.com>
2022-09-20 22:29:30 -0700

    more function changes, still not useable

--- assets/script.js
+++ assets/script.js
@@ -9,18 +9,18 @@ let seconds = 75
 let questions = [
     {
         quizQ: "Which of these values is not a JavaScript data type?"
-        options:["Boolean", "Number","ID","String"]
-        answer:"ID"
+        options: ["Boolean", "Number", "ID", "String"]
+        answer: "ID"
     }
     {
-        quizQ:"Which variable choice reassigns its value?" 
-        options:["Let", "Const"]
-        answer:"Let"
+        quizQ: "Which variable choice reassigns its value?" 
+        options: ["Let", "Const"]
+        answer: "Let"
     }
     {
         quizQ: "How do you select a single ID in JavaScript"
-        options:["getElementbyID", "createElement", "querySelectorAll", "addEventListenter"]
-        answer:"getElementbyID" 
+        options: ["getElementbyID", "createElement", "querySelectorAll", "addEventListenter"]
+        answer: "getElementbyID"
     }
     {
         quizQ: "Which section of the HTMl file should you use to link your CSS file?"
@@ -28,31 +28,46 @@ let questions = [
         answer: "Head"
     }
 ]
-    function createQuiz() {
-        start.setAttribute("style", "display:none")
-    }
+function createQuiz() {
+    start.setAttribute("style", "display:none")
+}
 
-    function beginTimer() {
-        const timerEnd = setInterval (function() {
-            seconds--;
-            time.textContent - seconds
-        }, 1000);
-    }
+function beginTimer() {
+    const timerEnd = setInterval(function () {
+        seconds--;
+        time.textContent - seconds
+    }, 1000);
+}
 
-    function renderQuestions() {
-        quizQuestion.innerHTML = ""
-        quizQuestion.textContent = questions[quizQuestion].quizQ
-        for (let i = 0; i < questions[quizQuestion].options.length; i++) {
-            let quizOption = questions[quizQuestion].options[i]
-            let button = document.createElement("button")
-            button.textContent = quizOption;
-            $(button.on("click", sumbit)
-            $(button.attr("data-value", quizOption)
-            
-
-
-            // let optionButton = document.createElement("button")
+function renderQuestions() {
+    quizQuestion.innerHTML = ""
+    quizQuestion.textContent = questions[quizQuestion].quizQ}
+    for (let i = 0; i < questions[quizQuestion].options.length; i++) {
+        let quizOption = questions[quizQuestion].options[i]
+        let button = document.createElement("button")
+        button.textContent = quizOption;
+        $(button.on("click", sumbit))
+            $(button.attr("data-value", quizOption))
         }
+    // let optionButton = document.createElement("button")
+
+    function nextQuiz() {
+        if (index < questions.length - 1) {
+            index++;
+            renderQuestions();
+        else {
+            clearInterval(timerEnd);
+            quizQuestion.innerHTML = "";
+            //highScore();
+            }
+            $(start.on("click", function(){
+                renderQuestions();
+                beginTimer();
+            })
+        }
+
+
+
     }
 
 
